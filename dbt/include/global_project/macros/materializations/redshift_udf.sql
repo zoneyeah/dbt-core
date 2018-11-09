3c3b8df2274b1ac851b4664175a69acd83e501ff
Connor McArthur <connor@fishtownanalytics.com>
2018-11-09 11:48:22 -0500

    done???

--- dbt/include/global_project/macros/materializations/redshift_udf.sql
+++ dbt/include/global_project/macros/materializations/redshift_udf.sql
@@ -1,20 +1,26 @@
 {% materialization udf, default %}
 
+    {%- set target_relation = api.Relation.create(
+      identifier=identifier,
+      schema=schema,
+      type='view') -%}
+
     -- put matching udf here
-    {%- set matching_udf = null %}
     {%- set identifier = model['alias'] -%}
 
-    {% call statement('find_udfs') %}
+    {% call statement('find_udfs', fetch_result=True) %}
         select * from pg_proc where proname ilike '%{{identifier}}%'
     {% endcall %}
 
-    {% if matching_udf %}
+    {% set matching_udfs = load_result('find_udfs')['data'] %}
+
+    {% if matching_udfs.length > 0 %}
         drop function {{ matching_udf }} cascade;
     {% endif %}
 
     {% call statement('main') %}
 
-        create function {{ model.alias }}
+        create function {{ target_relation }}
             (
                 {% for name, type in config.require('arguments').items() %}
                     {{name}} {{type}}
