3182bb69cdf1feae129ffda89adaad3e812c720c
Connor McArthur <connor@fishtownanalytics.com>
2019-11-08 13:11:36 -0500

    Bump version: 0.14.4b1 → 0.14.4

--- core/setup.py
+++ core/setup.py
@@ -9,7 +9,7 @@ def read(fname):
 
 
 package_name = "dbt-core"
-package_version = "0.14.4b1"
+package_version = "0.14.4"
 description = """dbt (data build tool) is a command line tool that helps \
 analysts and engineers transform data in their warehouse more effectively"""
 
