f39baaf61c52d7e4de63c9820c22f0f890a35345
Nathaniel May <nathaniel.may@fishtownanalytics.com>
2021-06-29 16:21:26 -0400

    Merge pull request #3497 from fishtown-analytics/experimental-parser-rust
    Experimental Parser: Swap python extractor for rust dependency

--- core/setup.py
+++ core/setup.py
@@ -73,10 +73,9 @@ setup(
         'networkx>=2.3,<3',
         'packaging~=20.9',
         'sqlparse>=0.2.3,<0.4',
-        'tree-sitter==0.19.0',
-        'tree-sitter-jinja2==0.1.0a1',
-        'typing-extensions>=3.7.4,<3.8',
-        'werkzeug>=0.15,<2.0',
+        'dbt-extractor==0.2.0',
+        'typing-extensions>=3.7.4,<3.11',
+        'werkzeug>=0.15,<3.0',
         # the following are all to match snowflake-connector-python
         'requests<3.0.0',
         'idna>=2.5,<3',
