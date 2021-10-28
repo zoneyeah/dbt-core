4fe1811aa170f671d5f4560bf98861076ce252e7
Jeremy Cohen <jeremy@dbtlabs.com>
2021-10-28 10:40:00 +0200

    Reorg materializations subdir

--- core/dbt/include/global_project/macros/materializations/models/incremental/merge.sql
+++ core/dbt/include/global_project/macros/materializations/models/incremental/merge.sql
@@ -65,6 +65,28 @@
 
 {%- endmacro %}
 
+{# here for backwards compatibility #}
+{% macro incremental_upsert(tmp_relation, target_relation, unique_key=none, statement_name="main") %}
+    
+    {%- set dest_columns = adapter.get_columns_in_relation(target_relation) -%}
+    {%- set dest_cols_csv = dest_columns | map(attribute='quoted') | join(', ') -%}
+
+    {%- if unique_key is not none -%}
+    delete
+    from {{ target_relation }}
+    where ({{ unique_key }}) in (
+        select ({{ unique_key }})
+        from {{ tmp_relation }}
+    );
+    {%- endif %}
+
+    insert into {{ target_relation }} ({{ dest_cols_csv }})
+    (
+       select {{ dest_cols_csv }}
+       from {{ tmp_relation }}
+    );
+{%- endmacro %}
+
 
 {% macro get_insert_overwrite_merge_sql(target, source, dest_columns, predicates, include_sql_header=false) -%}
   {{ adapter.dispatch('get_insert_overwrite_merge_sql', 'dbt')(target, source, dest_columns, predicates, include_sql_header) }}
