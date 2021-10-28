d7bb76dd72a0d1b1a0c1ef2a21282dcab7918a89
Jeremy Cohen <jeremy@dbtlabs.com>
2021-10-28 11:12:03 +0200

    Fix merge conflict

--- test/integration/016_macro_tests/test_macros.py
+++ test/integration/016_macro_tests/test_macros.py
@@ -207,7 +207,6 @@ class TestDispatchMacroOverrideBuiltin(TestMacroOverrideBuiltin):
     def test_postgres_overrides(self):
         self.run_dbt(["deps"])
         super().test_postgres_overrides()
-<<<<<<< HEAD
 
 
 class TestAdapterMacroDeprecated(DBTIntegrationTest):
@@ -233,5 +232,3 @@ class TestAdapterMacroDeprecated(DBTIntegrationTest):
             self.run_dbt(['run'])
 
         assert 'The "adapter_macro" macro has been deprecated' in str(exc.value)
-=======
->>>>>>> 343402ad (Add integration tests)
