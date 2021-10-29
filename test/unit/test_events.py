1e07443b8810c96f9599f50b5a1eff9e77d11068
Nathaniel May <nathaniel.may@fishtownanalytics.com>
2021-10-29 09:39:45 -0400

    add test for documented adapter maintainer interface

---
+++ test/unit/test_events.py
@@ -0,0 +1,18 @@
+from unittest import mock, TestCase
+
+
+class TestFlags(TestCase):
+
+    def setUp(self):
+        pass
+
+    # this interface is documented for adapter maintainers to plug into
+    # so we should test that it at the very least doesn't explode.
+    def test_adapter_logging_interface(self):
+        from dbt.events import AdapterLogger
+        logger = AdapterLogger("dbt_tests")
+        logger.debug("debug message")
+        logger.info("info message")
+        logger.warning("warning message")
+        logger.error("error message")
+        self.assertTrue(True)
