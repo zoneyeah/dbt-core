e98d658695993542a0780b638ea4b0aedbd420f5
Emily Rockman <emily.rockman@dbtlabs.com>
2021-11-01 11:31:21 -0500

    fixed merge error

--- core/dbt/events/types.py
+++ core/dbt/events/types.py
@@ -297,6 +297,9 @@ class MacroEventDebug(DebugLevel, CliEventABC):
 
     def cli_msg(self) -> str:
         return self.msg
+
+
+@dataclass
 class MissingProfileTarget(InfoLevel, CliEventABC):
     profile_name: str
     target_name: str
