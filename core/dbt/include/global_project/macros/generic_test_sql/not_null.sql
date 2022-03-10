c251dae75eaf7ee5d0e21b54d27701722905e96b
willbowditch <14288042+willbowditch@users.noreply.github.com>
2022-03-10 02:31:15 +0000

    [CT-271] [Feature] not_null test selects column instead of * (#4777)
    * Only select target column for not_null test
    
    * If storing failures include all columns in the select, if not, only select the column being tested
    
    It's desirable for this test to include the full row output when using --store-failures. If the query result stored in the database contained just the null values of the null column, it can't do much to contextualize why those rows are null.
    
    * Update changelog
    
    * chore: update changelog using changie
    
    * Revert "Update changelog"
    
    This reverts commit 281d805959e15694784cfa3a078fc5ef059c06b5.

--- core/dbt/include/global_project/macros/generic_test_sql/not_null.sql
+++ core/dbt/include/global_project/macros/generic_test_sql/not_null.sql
@@ -1,6 +1,8 @@
 {% macro default__test_not_null(model, column_name) %}
 
-select *
+{% set column_list = '*' if should_store_failures() else column_name %}
+
+select {{ column_list }}
 from {{ model }}
 where {{ column_name }} is null
 
