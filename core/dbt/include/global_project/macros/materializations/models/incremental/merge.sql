ea5a9da71ef71cc13414cd8c2f8097448add5edb
Matthew McKnight <91097623+McKnight-42@users.noreply.github.com>
2022-03-22 10:24:21 -0500

    update of macro for postgres/redshift use of unique_key as a list (#4858)
    * pre-commit additions
    
    * added changie changelog entry
    
    * moving integration test over
    
    * Pair programming
    
    * removing ref to mapping as seems to be unnecessary check, unique_key tests pass locally for postgres
    
    Co-authored-by: Jeremy Cohen <jeremy@dbtlabs.com>

--- core/dbt/include/global_project/macros/materializations/models/incremental/merge.sql
+++ core/dbt/include/global_project/macros/materializations/models/incremental/merge.sql
@@ -56,13 +56,26 @@
 
     {%- set dest_cols_csv = get_quoted_csv(dest_columns | map(attribute="name")) -%}
 
-    {% if unique_key is not none %}
-    delete from {{ target }}
-    where ({{ unique_key }}) in (
-        select ({{ unique_key }})
-        from {{ source }}
-    );
-    {% endif %}
+    {% if unique_key %}
+        {% if unique_key is sequence and unique_key is not string %}
+            delete from {{target }}
+            using {{ source }}
+            where (
+                {% for key in unique_key %}
+                    {{ source }}.{{ key }} = {{ target }}.{{ key }}
+                    {{ "and " if not loop.last }}
+                {% endfor %}
+            );
+        {% else %}
+            delete from {{ target }}
+            where (
+                {{ unique_key }}) in (
+                select ({{ unique_key }})
+                from {{ source }}
+            );
+
+        {% endif %}
+        {% endif %}
 
     insert into {{ target }} ({{ dest_cols_csv }})
     (
